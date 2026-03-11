1fe3598cbe3bc4135918c3bd99e00d1e5b8df5f1
Julio Cesar <juliocesaar.ma@gmail.com>
2026-03-11 18:45:35 -0300

    chore: add debug env endpoint

---
+++ api/debug-env.ts
@@ -0,0 +1,13 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(_req: VercelRequest, res: VercelResponse) {
+  const mpToken = process.env.MP_ACCESS_TOKEN;
+  return res.status(200).json({
+    has_mp_token: !!mpToken,
+    mp_token_prefix: mpToken ? mpToken.slice(0, 10) + "..." : "undefined",
+    has_resend: !!process.env.RESEND_API_KEY,
+    has_site_url: !!process.env.SITE_URL,
+    node_env: process.env.NODE_ENV,
+    vercel_env: process.env.VERCEL_ENV,
+  });
+}
