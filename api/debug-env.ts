d9863580ae78ba98b0f0db5f78012c174f25a972
Julio Cesar <juliocesaar.ma@gmail.com>
2026-03-11 19:43:05 -0300

    fix: direct import from build-env, skip load-env

--- api/debug-env.ts
+++ api/debug-env.ts
@@ -1,9 +1,15 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { loadEnv } from "./_lib/load-env";
+import { ENV } from "./_lib/build-env";
 
 export default function handler(_req: VercelRequest, res: VercelResponse) {
-  loadEnv();
+  for (const [key, value] of Object.entries(ENV)) {
+    if (!process.env[key]) {
+      process.env[key] = value;
+    }
+  }
+
   return res.status(200).json({
+    env_keys: Object.keys(ENV),
     has_mp_token: !!process.env.MP_ACCESS_TOKEN,
     has_resend: !!process.env.RESEND_API_KEY,
     has_site_url: !!process.env.SITE_URL,
